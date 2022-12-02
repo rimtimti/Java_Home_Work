f11ab17d5fd191d20bad2716aa79ba9977924cac
rimtimti <rimtimti@mail.ru>
2022-12-02 17:40:15 +0300

    OOPseminar3-1

--- OOP_Seminar3/Main.java
+++ OOP_Seminar3/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         CustomList<User> cl = new CustomList<>();
         cl.add(new User("Сергей", "Петров", 23));
-        cl.add(new User("Лев", "Толстой", 100));
-        cl.add(new User("Александр", "Пушкин", 23));
+        cl.add(new User("Иван", "Иванов", 78));
+        cl.add(new User("Игорь", "Сидоров", 23));
         for (final Node node : cl) {
             System.out.println(node.getData());
         }
